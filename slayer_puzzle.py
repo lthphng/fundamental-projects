5ea5b66999b28d72f9e490d180458671ce811bb9
lthphng <114843071+lthphng@users.noreply.github.com>
2023-02-01 21:20:46 +0700

    Delete slayer_puzzle.py

--- slayer_puzzle.py
+++
@@ -1,9 +0,0 @@
-print("Solve: SLAYER + SLAYER + SLAYER = LAYERS")
-
-a = int(input("What is your guess? "))
-b = a//100000 + (a-a//100000*100000)*10
-
-if b == a*3:
-    print(b, "==", a*3, "-> True")
-else:
-    print(b, "==", a*3, "-> False")
